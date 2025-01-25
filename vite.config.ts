780f88eea7adf7495f1a88196d8f229a6417e8b0
Xrayya <nur.alkhiyar@gmail.com>
2025-01-25 20:27:54 +0700

    chore(deps): setup tanstack router and shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
+});
