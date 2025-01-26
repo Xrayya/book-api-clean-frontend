ee04b10830020d24ce57b8bb0e7d075fb4bd39ff
Xrayya <nur.alkhiyar@gmail.com>
2025-01-26 11:03:42 +0700

    feat(sidebar): initiate

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,35 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+
+type AppSidebarProps = {
+  sidebarMenuItems: React.ReactNode[];
+};
+
+export const AppSidebar: React.FC<AppSidebarProps> = ({ sidebarMenuItems }) => {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {sidebarMenuItems.map((item, idx) => (
+                <SidebarMenuItem key={idx}>
+                  <SidebarMenuButton asChild>{item}</SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,32 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import "../global.css"
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Home } from "lucide-react";
+import "../global.css";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: Root,
+});
+
+function Root() {
+  const sidebarMenuItems: React.ReactNode[] = [
+    <Link to={"/"}>
+      <Home />
+      Home
+    </Link>,
+  ];
+
+  return (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
+      <SidebarProvider>
+        <AppSidebar sidebarMenuItems={sidebarMenuItems} />
+        <main>
+          <SidebarTrigger />
+          <Outlet />
+        </main>
+      </SidebarProvider>
       <TanStackRouterDevtools />
     </>
-  ),
-})
+  );
+}
